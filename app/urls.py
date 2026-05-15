2492bca30722b554bf7ec0ce7e0a3c4748b1b90c
Henrique Cals <henrique.cals6@gmail.com>
2026-05-15 08:37:26 -0300

    Sua mensagem aqui, ex: Adiciona novo menu

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),  # URLs tradicionais
+    path('api/', include('app.api_urls')),  # URLs da API
 ]
