54e4cb7d1287eaacde966216ea685921ee27f9c1
PEDRO PAULO DE SOUSA OLIVEIRA <202508404648@alunos.ibmec.edu.br>
2026-06-09 16:21:15 -0300

    perf: eliminar N+1 queries com select/prefetch_related (R-06)
    - ContentViewSet: select_related('creator') para evitar N queries adicionais
    - PlaylistViewSet: prefetch_related('contents__creator') para otimizar aninhamento
    - Adicionar atributos filter_backends, search_fields, ordering_fields aos ViewSets
    - Remover import desnecessário 'from django.shortcuts import render'
    - Fechar NC-07, NC-08, NC-15

--- content_app/views.py
+++ content_app/views.py
@@ -1,26 +1,35 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Content, Playlist
 from .serializers import ContentSerializer, PlaylistSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
+
 class ContentViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
+    queryset = Content.objects.select_related('creator').all()
     serializer_class = ContentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['content_type', 'is_public', 'status']
+    search_fields = ['title', 'description']
+    ordering_fields = ['upload_date', 'views', 'likes']
+    ordering = ['-upload_date']
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
 
 
 class PlaylistViewSet(viewsets.ModelViewSet):
-    queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
     permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at', 'updated_at']
+    ordering = ['-created_at']
 
     def get_queryset(self):
         # Permite que o usuário veja apenas suas próprias playlists
-        return self.queryset.filter(user=self.request.user)
+        return Playlist.objects.prefetch_related('contents__creator').filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
